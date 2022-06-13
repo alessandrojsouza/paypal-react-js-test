ee42889b2417175ff951710e43c0292209efefa6
Alessandro Souza <alessandrojsouza@gmail.com>
2022-06-12 22:49:25 -0300

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+
+//Ref documentation
+// https://paypal.github.io/react-paypal-js/?path=/story/getting-started--page
+
+
+//imports
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+//config sdk paypal
+const initialOptions = {
+  "client-id": "ATLNTVnFKdQJVXmhETGiTSmvA4BvzxZvYZRTXXyyao6hlJmK2O2UpAxpKqka1v03EIlC5QtSWWlX2h_K",
+  currency: "BRL",
+ // intent: "capture",
+ // "data-client-token": "abc123xyz==",
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PayPalScriptProvider options={initialOptions}>
+        <PayPalButtons
+            createOrder={(data, actions) => {
+                return actions.order.create({
+                    purchase_units: [
+                        {
+                            amount: {
+                                value: "1.99", //transaction value (can be variable)
+                            },
+                        },
+                    ],
+                });
+            }}
+            onApprove={(data, actions) => {
+                return actions.order.capture().then((details) => {
+                    const name = details.payer.name.given_name;
+                   //const name = details.id
+                    alert(`Transaction completed by ${name}`);
+                });
+            }}
+        />
+    </PayPalScriptProvider>
   );
 }
 
